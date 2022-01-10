ef9715f1dc3fc1c24f7d328e848a612cd38e9475
Vance Winstead <vance.winstead@gmail.com>
2022-01-09 22:15:01 -0800

    Added MongoDB and .env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// Start Express Server
 const app = express();
 app.use(express.json());
+
+// Connect to MongoDB
+if (!process.env.CONNECTION_URL) {
+  console.warn("Missing CONNECTION_URL environment variable");
+}
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => console.log("Succfully connected to mongodb"))
+  .catch((error) => console.log(`Could not connect due to ${error}`));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
