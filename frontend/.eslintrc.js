2e4ec2e60f18a52f4e0156f7411bf41429b26ebb
hayahann <hhayashi@calpoly.edu>
2022-01-17 10:45:36 -0800

    added rules and overrides to .eslintrc file

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,21 +3,28 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb',
-    "prettier",
-  ],
+  extends: ["airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    "prettier",
-  ],
+  plugins: ["prettier"],
   rules: {
     "prettier/prettier": "error",
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
   },
+  overrides: [
+    {
+      files: ["**/*.test.js", "**/*.test.jsx"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
