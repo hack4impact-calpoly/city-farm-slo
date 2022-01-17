20d0336a1b9c4b181bd66a77feec555a6d6c1c26
Vance Winstead <vance.winstead@gmail.com>
2022-01-17 12:45:35 -0800

    Removed Calendar from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Calendar from "./components/Calendar";
 
 function App() {
   return (
     <div>
       <p>Welcome to City Farm SLO!</p>
-      <Calendar />
     </div>
   );
 }
