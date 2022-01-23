150f7d8dfac3090d1105b9e34314c24fc908f3fa
ryanchansf <ryanchansf@gmail.com>
2022-01-22 23:06:16 -0800

    Finished RegistrationComplete component
    Created RegistrationComplete component based on Figma mockup. Linked "Return to City Farm SLO" button to home page.

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import RegistrationComplete from "./RegistrationComplete/RegistrationComplete";
 
 export default function Home() {
   return (
     <div>
       <p>Welcome to City Farm SLO!</p>
+      <RegistrationComplete />
     </div>
   );
 }
