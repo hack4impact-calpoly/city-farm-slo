972452800e7a776743cd2bad88424942a4fe8fb5
Adelle Vo <adelle.vo@gmail.com>
2022-01-23 17:40:22 -0800

    finished styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 
 import Home from "./components/home";
+import SignUpForm from "./components/UserSignUp/SignUpForm";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <StylesProvider injectFirst>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+              <SignUpForm />
+              {/* <SignUpForm2/> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </StylesProvider>
   );
 }
 
