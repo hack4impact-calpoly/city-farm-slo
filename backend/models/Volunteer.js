c7c73fd2780957b010eaf0c4fb2335570035066f
vboo123 <89175545+vboo123@users.noreply.github.com>
2022-01-24 18:51:48 -0800

    Volunteer Model
    Implementation of the Volunteer Model, no routes

---
+++ backend/models/Volunteer.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const VolunteerSchema = new mongoose.schema(
+  {
+      firstName: firstName,
+      lastName: lastName, 
+      email: email,
+      phone: phone, 
+      notes: notes,
+    },
+);
+const Volunteer = mongoose.model("Volunteer", VolunteerSchema);
+
+module.exports = Volunteer;
