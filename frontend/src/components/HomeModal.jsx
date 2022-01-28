afd2342a082897aa29d5ce14fcb2bcb5019ddbcf
ryyhu <ryanhu021@gmail.com>
2022-01-27 21:13:28 -0800

    Add device type checking

--- frontend/src/components/HomeModal.jsx
+++ frontend/src/components/HomeModal.jsx
@@ -2,26 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Modal from "@mui/material/Modal";
+import { isMobile } from "react-device-detect";
 
 export default function HomeModal({ children, open, handleClose }) {
-  const ModalContainer = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    min-width: 400px;
-    background-color: white;
-    border-radius: 12px;
-    padding: 8px;
-  `;
+  const ModalContainer = isMobile
+    ? styled.div`
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        min-width: 80%;
+        background-color: white;
+        border-radius: 12px;
+        padding: 8px;
+      `
+    : styled.div`
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        min-width: 20%;
+        background-color: white;
+        border-radius: 12px;
+        padding: 8px;
+      `;
 
   return (
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
+    <Modal open={open} onClose={handleClose}>
       <ModalContainer>{children}</ModalContainer>
     </Modal>
   );
