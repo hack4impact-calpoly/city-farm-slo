78b7019fa89393fc798c155dbdbb7f549428a110
Adelle Vo <adelle.vo@gmail.com>
2022-01-31 04:02:39 -0800

    get & post

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Event = require("./models/event");
 require("dotenv").config();
 
 // Start Express Server
@@ -17,6 +18,38 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+// #1 - get all events
+app.get("/api/events", async (req, res) => {
+  try {
+    const events = await Event.find({});
+    res.send(events);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(`error is ${error.message}`);
+  }
+});
+
+// #2 - add an event
+app.post("/api/recipe", async (req, res) => {
+  try {
+    const { title, Location, start, end, slots, notes, Volunteers } = req.body;
+    let event = new Event({
+      title,
+      Location,
+      start,
+      end,
+      slots,
+      notes,
+      Volunteers,
+    });
+    event = await event.save();
+    res.json(event);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(`error is ${error.message}`);
+  }
 });
 
 app.listen(3001);
