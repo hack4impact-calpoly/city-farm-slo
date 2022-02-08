26335e25e767c853d2c527d4feeff123f5413dd1
Vance Winstead <vance.winstead@gmail.com>
2022-02-07 21:51:39 -0800

    Moved StyledComponents out of Functions

--- frontend/src/components/UserSignUp/Calendar.jsx
+++ frontend/src/components/UserSignUp/Calendar.jsx
@@ -8,17 +8,17 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 // - Edit the Home component to look like this Figma Mockup: https://www.figma.com/file/AU4DBu8GtFmivr0q971PpY/cityfarmslo-high-fidel?node-id=4%3A18
 // - Please use styled-components. If you have time, find a calendar component to use on this page (I'd reccomend looking at FullCalendar, react-calendar, or react-datepicker).
 
-export default function Calendar({ events }) {
-  const CalendarWrapper = styled.div`
-    padding: 36px;
-    width: 900px;
-    border: 1px solid #090a0c;
-    box-sizing: border-box;
-    box-shadow: 0px 4px 32px rgba(170, 170, 170, 0.03);
-    border-radius: 12px;
-    height: 100%;
-  `;
+const CalendarWrapper = styled.div`
+  padding: 36px;
+  width: 900px;
+  border: 1px solid #090a0c;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 32px rgba(170, 170, 170, 0.03);
+  border-radius: 12px;
+  height: 100%;
+`;
 
+export default function Calendar({ events }) {
   const handleEventClick = (clickInfo) => {
     const { title, start, end } = clickInfo.event;
     const { slots, volunteers } = clickInfo.event.extendedProps;
