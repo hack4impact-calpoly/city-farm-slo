5f683eceb472b4cbfe6dd333529f7f95b24fd9f2
Adelle Vo <adelle.vo@gmail.com>
2022-02-13 08:35:45 -0800

    events schema validation

--- backend/models/volunteer.js
+++ backend/models/volunteer.js
@@ -1,13 +1,32 @@
 const mongoose = require("mongoose");
 
 const VolunteerSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  phone: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: /.+@.+\..+/,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /\d{3}\d{3}\d{4}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number.`,
+    },
+    required: [true, `Phone number required.`],
+  },
   notes: String,
 });
 
 const Volunteer = mongoose.model("Volunteer", VolunteerSchema);
-
 module.exports = Volunteer;
--- backend/routes/event.js
+++ backend/routes/event.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { body, validationResult, check } = require("express-validator");
 const Event = require("../models/event");
 
 const router = express.Router();
+
+const app = express();
+app.use(express.json());
 
 // #1 - get all events
 router.get("/", async (req, res) => {
@@ -15,24 +19,40 @@ router.get("/", async (req, res) => {
 });
 
 // #2 - add an event
-router.post("/add", async (req, res) => {
-  try {
-    const { title, location, start, end, slots, notes } = req.body;
-    let event = new Event({
-      title,
-      location,
-      start,
-      end,
-      slots,
-      notes,
-      volunteers: [],
-    });
-    event = await event.save();
-    res.json(event);
-  } catch (error) {
-    res.status(500).send(error.message);
-    console.log(`error is ${error.message}`);
+router.post(
+  "/add",
+  check("title").isString().withMessage("Not a string"),
+  check("location").isString().withMessage("Not a string"),
+  check("start").trim().isISO8601().toDate().withMessage("Not a date"),
+  check("end").trim().isISO8601().toDate().withMessage("Not a date"),
+  check("slots").isInt().withMessage("Not a valid number"),
+  check("notes")
+    .if(body("notes").notEmpty())
+    .isString()
+    .withMessage("Not a string"),
+  check("volunteers").isArray().withMessage("Not an array"),
+  // eslint-disable-next-line consistent-return
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      await Event.create({
+        title: req.body.title,
+        location: req.body.location,
+        start: req.body.start,
+        end: req.body.end,
+        slots: req.body.slots,
+        notes: req.body.notes,
+        volunteers: [],
+      }).then((event) => res.json(event));
+    } catch (error) {
+      res.status(500).send(error.message);
+      console.log(`error is ${error.message}`);
+    }
   }
-});
+);
 
 module.exports = router;
