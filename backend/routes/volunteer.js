de64eedc12c83326cafc4dddf5d90be9271fe0d3
Vance Winstead <vance.winstead@gmail.com>
2022-02-16 15:39:48 -0800

    Add volunteer to events

--- backend/routes/volunteer.js
+++ backend/routes/volunteer.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const Volunteer = require("../models/volunteer");
+const Event = require("../models/event");
 
 const router = express.Router();
 
 // #1 - get all volunteers
 router.get("/:id", async (req, res) => {
   try {
-    let volunteer = await Volunteer.findById(req.params.id);
-    res.send({ volunteer });
+    const volunteer =
+      (await Volunteer.findById(req.params.id)) ?? "Volunteer not found";
+    res.send(volunteer);
   } catch (error) {
     res.status(500).send(error.message);
     console.log(`error is ${error.message}`);
@@ -17,15 +19,24 @@ router.get("/:id", async (req, res) => {
 // #2 - add a volunteer
 router.post("/register", async (req, res) => {
   try {
-    const { firstName, lastName, email, phone } = req.body;
+    const { firstName, lastName, email, phone } = req.body.volunteer;
+    const { eventID } = req.body;
+
+    if (!eventID) {
+      res.status(400).send("Missing eventID");
+      return;
+    }
+
     let volunteer = new Volunteer({
       firstName,
       lastName,
       email,
       phone,
     });
-
     volunteer = await volunteer.save();
+    await Event.findByIdAndUpdate(eventID, {
+      $push: { volunteers: volunteer._id },
+    });
     res.json(volunteer);
   } catch (error) {
     res.status(500).send(error.message);
