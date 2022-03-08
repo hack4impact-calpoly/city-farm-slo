e76f8e2f2f3bacea07ab9e56ef23dc3028affafa
ryyhu <ryanhu021@gmail.com>
2022-03-07 19:51:37 -0800

    Update volunteer model for under 18

--- backend/models/volunteer.js
+++ backend/models/volunteer.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const VolunteerSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  notes: { type: String, required: false },
+  email: { type: String, required: true },
+  phone: { type: String },
+  isAdult: { type: Boolean },
+  signedWaiver: { type: Boolean, default: false },
+  dateSigned: { type: Date },
+  parentName: { type: String },
 });
 
 const Volunteer = mongoose.model("Volunteer", VolunteerSchema);
