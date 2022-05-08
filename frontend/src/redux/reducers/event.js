7cf05709c474e1fa54eddcbc6423ce198790a664
ryyhu <ryanhu021@gmail.com>
2022-05-08 14:41:38 -0700

    Fix other reducers

--- frontend/src/redux/reducers/event.js
+++ frontend/src/redux/reducers/event.js
@@ -41,8 +41,10 @@ const editEvent = (event) => async (dispatch) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(event),
-  }).catch((err) => console.log(err));
-  dispatch(editEventReducer(event));
+  })
+    .then((res) => res.json())
+    .then((data) => dispatch(editEventReducer(data)))
+    .catch((err) => console.log(err));
 };
 
 const deleteEvent = (event) => async (dispatch) => {
@@ -52,8 +54,10 @@ const deleteEvent = (event) => async (dispatch) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(event),
-  }).catch((err) => console.log(err));
-  dispatch(deleteEventReducer(event));
+  })
+    .then((res) => res.json())
+    .then((data) => dispatch(deleteEventReducer(data)))
+    .catch((err) => console.log(err));
 };
 
 export { fetchEvents, addEvent, editEvent, deleteEvent };
